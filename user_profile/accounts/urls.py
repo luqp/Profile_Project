341b54cc04e6c99e8b85058eb5e2fd9788d14807
windyludev <windyludev@outlook.com>
2019-06-24 23:32:14 -0400

    Update the views

--- user_profile/accounts/urls.py
+++ user_profile/accounts/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('sign_in/', views.sign_in, name='sign_in'),
     path('sign_up/', views.sign_up, name='sign_up'),
     path('sign_out/', views.sign_out, name='sign_out'),
+    path('password/', views.change_password, name='password'),
+    path('profile/', views.profile_detail, name='profile_detail'),
+    path('profile/edit', views.profile_edit, name='profile_edit'),
 ]
--- user_profile/accounts/views.py
+++ user_profile/accounts/views.py
@@ -1,13 +1,32 @@
+import glob
+import random
+
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django.shortcuts import render
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
-from .admin import UserCreationForm
+from django.contrib.auth import (authenticate, login, logout, 
+                                 update_session_auth_hash)
+from . import forms
 
 
-from django.shortcuts import render
+def select_user_img(user):
+    try:
+        path = user.avatar.url
+    except ValueError:
+        path = select_ramdon_image()
+    return path
+
+
+def select_ramdon_image():
+    url_images = []
+    for path in glob.glob('media/imgs/*.svg'):
+        path = '/' + path
+        url_images.append(path)
+    url = random.choice(url_images)
+    return url
 
 
 def sign_in(request):
@@ -19,9 +38,7 @@ def sign_in(request):
                 user = form.user_cache
                 if user.is_active:
                     login(request, user)
-                    return HttpResponseRedirect(
-                        reverse('home') # TODO: go to profile
-                    )
+                    return HttpResponseRedirect(reverse('accounts:profile_detail'))
                 else:
                     messages.error(
                         request,
@@ -36,9 +53,9 @@ def sign_in(request):
 
 
 def sign_up(request):
-    form = UserCreationForm()
+    form = forms.UserCreationForm()
     if request.method == 'POST':
-        form = UserCreationForm(data=request.POST)
+        form = forms.UserCreationForm(data=request.POST)
         if form.is_valid():
             form.save()
             user = authenticate(
@@ -50,7 +67,7 @@ def sign_up(request):
                 request,
                 "You're now a user! You've been signed in, too."
             )
-            return HttpResponseRedirect(reverse('home'))  # TODO: go to profile
+            return HttpResponseRedirect(reverse('accounts:profile_detail'))
     return render(request, 'accounts/sign_up.html', {'form': form})
 
 
@@ -59,3 +76,57 @@ def sign_out(request):
     logout(request)
     messages.success(request, "You've been signed out. Come back soon!")
     return HttpResponseRedirect(reverse('home'))
+
+
+@login_required
+def profile_detail(request):
+    user = request.user
+    path = select_user_img(user)
+    return render(request, 'accounts/profile_detail.html', {
+        'user': user,
+        'path': path,
+    })
+
+
+@login_required
+def profile_edit(request):
+    user = request.user
+    form = forms.ProfileForm(instance=user)
+    if request.method == 'POST':
+        form = forms.ProfileForm(
+            instance=user, data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request,
+                "You saved successful your data"
+            )
+            return HttpResponseRedirect(reverse('accounts:profile_detail'))
+    path = select_user_img(user)
+    return render(request, 'accounts/profile_form.html', {
+        'form': form,
+        'title': 'Edit Profile',
+        'path': path,
+    })
+
+
+@login_required
+def change_password(request):
+    user = request.user
+    form = forms.CustomPasswordChangeForm(user)
+    if request.method == 'POST':
+        form = forms.CustomPasswordChangeForm(user=user, data=request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(
+                request, 'Your password was successfully updated!')
+            return HttpResponseRedirect(reverse('accounts:profile_detail'))
+        else:
+            messages.error(request, 'Please correct the error below.')
+    path = select_user_img(user)
+    return render(request, 'accounts/password_form.html', {
+        'form': form,
+        'title': 'Change password',
+        'path': path,
+    })
