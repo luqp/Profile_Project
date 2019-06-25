9f533d1e6f8e1801ecf9b2b291fe37e604f0f47e
windyludev <windyludev@outlook.com>
2019-06-24 22:57:32 -0400

    Setup the path to the views

--- user_profile/user_profile/urls.py
+++ user_profile/user_profile/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -26,4 +28,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls', namespace='accounts'))
 ]
 
-urlpatterns += staticfiles_urlpatterns() 
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
